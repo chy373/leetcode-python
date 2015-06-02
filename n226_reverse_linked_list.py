36655a81d223a94a79969a15b53c2f4ad0f30f36
cy <chy373@gmail.com>
2015-06-02 10:32:45 +0800

    fix reverse a linked list iteractively

--- n226_reverse_linked_list.py
+++ n226_reverse_linked_list.py
@@ -16,13 +16,28 @@ class Solution:
         prev = None
         while head:
             curr = head
-            curr.next = prev
             head = head.next
+            curr.next = prev
             prev = curr
+            #a simple trick : head.next, head, prev = prev, head.next, head
         return prev
 
     def reverseListRecursive(self, head, prev=None):
-        if not node:
+        if not head:
             return prev
-        head = head.next
+        next = head.next
         head.next = prev
+        return self.reverseListRecursive(next, head)
+
+if __name__ == '__main__':
+    l1 = ListNode(0)
+    l1.next = ListNode(1)
+    l1.next.next = ListNode(2)
+    l2 = ListNode(7)
+    l2.next = ListNode(8)
+    l2.next.next = ListNode(9)
+    a = Solution()
+    print 'origin linked list {}'.format(l1)
+    print 'after iterative solution {}'.format(a.reverseListIterative(l1))
+    print 'origin linked list {}'.format(l2)
+    print 'after recursive solution {}'.format(a.reverseListRecursive(l2))
